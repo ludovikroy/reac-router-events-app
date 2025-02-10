f1aa652487cd0bd81caf3c0e0fcb852d1cc74b0f
Ludovik Roy <ludovik.roy@cgi.com>
2025-02-10 16:55:34 -0500

    Initial commit

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import PageContent from "../components/PageContent";
+
+function HomePage() {
+  return (
+    <PageContent title="Welcome!">
+      <p>Brownse all our amazing events!</p>
+    </PageContent>
+  );
+}
+
+export default HomePage;
